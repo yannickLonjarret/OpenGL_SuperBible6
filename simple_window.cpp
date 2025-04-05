882b3ca74b2cf399b85cb641b75fb55d3fc921d0
yannickLonjarret <yannick.lonjarret@gmail.com>
2025-04-05 11:48:03 +0200

    Added window manager class and made window dynamic

--- simple_window.cpp
+++ simple_window.cpp
@@ -5,47 +5,99 @@
 #include <glad/glad.h> 
 #include <iostream>
 #include "GLFW/glfw3.h"
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-void listenProgramEnd(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-int main()
-{
+class WindowManager {
+private:
+    struct DestroyglfwWin {
 
+        void operator()(GLFWwindow* ptr) {
+            glfwDestroyWindow(ptr);
+        }
+
+    };
+private:
+    std::unique_ptr<GLFWwindow, DestroyglfwWin> managedOpenGLWindow;
+
+
+public:
+
+    WindowManager(){
+        managedOpenGLWindow = std::unique_ptr<GLFWwindow, DestroyglfwWin>(glfwCreateWindow(500, 500, "My first window in OpenGL", NULL, NULL));
+
+        if (!managedOpenGLWindow.get())
+            throw(std::exception("Failed to open window"));
+        
+        glfwMakeContextCurrent(managedOpenGLWindow.get());
+        glfwSetFramebufferSizeCallback(managedOpenGLWindow.get(), FramebufferSizeCallback);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+            throw(std::exception("Failed to initialize GLAD"));
+        
+    }
+
+    GLFWwindow* GetManagedWindowPointer() const {
+        return managedOpenGLWindow.get();
+    }
+
+    void Render(const double colorValueToProcess) const {
+        GLfloat colors[] = { std::cos(colorValueToProcess), std::sin(colorValueToProcess), colorValueToProcess, 1.0f};
+        glClearBufferfv(GL_COLOR, 0, colors) ;
+        glfwSwapBuffers(managedOpenGLWindow.get());
+    }
+
+    void ListenProgramEnd()
+    {
+        if (glfwGetKey(managedOpenGLWindow.get(), GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(managedOpenGLWindow.get(), true);
+    }
+
+    void ProcessEvents() {
+        glfwPollEvents();
+    }
+
+    bool EndWindow() {
+        return !glfwWindowShouldClose(managedOpenGLWindow.get());
+    }
+
+    
+};
+
+
+void InitializeOpenGLToolkit() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwInit();
+}
 
-    GLFWwindow* win = glfwCreateWindow(500, 500, "My first window in OpenGL", NULL, NULL);
-    if (!win)
-    {
-        std::cout<< "Failed to open window" <<std::endl;
-        return -1;
-    }
-
-    glfwMakeContextCurrent(win);
-    glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-    do {
-        listenProgramEnd(win);
-        static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-        glClearBufferfv(GL_COLOR, 0, red);
-        glfwSwapBuffers(win);
-        glfwPollEvents();
-    } while (!glfwWindowShouldClose(win));
-   
+void EndOpenGLToolkit() {
     glfwTerminate();
+}
+int main()
+{
+    InitializeOpenGLToolkit();
+
+    WindowManager window = WindowManager();
+
+    double smallSteps = 0.;
+    while (window.EndWindow()){
+
+        window.ListenProgramEnd();
+        window.Render(smallSteps);
+        window.ProcessEvents();
+
+        smallSteps += 1.e-4;
+        if (smallSteps > _HUGE_ENUF)
+            smallSteps = 0.0;
+ 
+    } 
+    EndOpenGLToolkit();
     return 0;
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
