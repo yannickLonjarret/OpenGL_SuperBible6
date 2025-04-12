56e6b07b2fd48ecd53f64a4371558ec5ff2b8375
yannickLonjarret <yannick.lonjarret@gmail.com>
2025-04-12 21:33:09 +0200

    Create triangle shader class

--- SinglePointShader.h
+++ SinglePointShader.h
@@ -59,34 +59,28 @@ namespace Shaders {
 	public:
 		TriangleShader() = default;
 
-		TriangleShader(const GLchar* vertex, const GLchar* fragment) {
+		TriangleShader(const GLchar* vertex) {
 
 			GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 			glShaderSource(vertexShader, 1, &vertex, NULL);
 			glCompileShader(vertexShader);
 
-			GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-			glShaderSource(fragmentShader, 1, &fragment, NULL);
-			glCompileShader(fragmentShader);
-
 			this->shader = glCreateProgram();
 			glAttachShader(this->shader, vertexShader);
-			glAttachShader(this->shader, fragmentShader);
+			
 			glLinkProgram(this->shader);
 
 			glDeleteShader(vertexShader);
-			glDeleteShader(fragmentShader);
-
+			
 			glGenVertexArrays(1, &vertexArrayObject);
 			glBindVertexArray(vertexArrayObject);
 		}
 
 		void SetPointSize(float newSize) const { glPointSize(newSize); }
 
-		void DrawPoint() const {
-			glBindVertexArray(vertexArrayObject);
+		void DrawTriangle() const {
 			glUseProgram(this->shader);
-			glDrawArrays(GL_POINTS, 0, 1);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		}
 
