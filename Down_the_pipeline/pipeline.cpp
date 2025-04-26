2b5b70b7525acad45cb9f6466e22049c0968c487
yannickLonjarret <yannick.lonjarret@gmail.com>
2025-04-26 11:16:26 +0200

    Chapter 2 start

---
+++ Down_the_pipeline/pipeline.cpp
@@ -0,0 +1,26 @@
+
+#pragma once
+#include "WindowManager.h"
+#include <cmath>
+#include <chrono>
+#include <thread>
+
+
+int main()
+{
+    WindowManagement::WindowManager window = WindowManagement::WindowManager();
+
+    double smallSteps = 0.;
+    while (window.EndWindow()) {
+
+        window.ListenProgramEnd();
+        window.Render(smallSteps);
+        window.ProcessEvents();
+
+        smallSteps += 5.e-4;
+        if (smallSteps > _HUGE_ENUF)
+            smallSteps = 0.0;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    return 0;
+}
